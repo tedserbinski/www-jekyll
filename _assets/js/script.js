42eb34ed912826a3f121e0950f83d212ffa4afdb
ted serbinski <stanson@gmail.com>
2014-07-15 15:17:14 -0400

    load twitter sharing after rest of page loads

--- _assets/js/script.js
+++ _assets/js/script.js
@@ -45,3 +45,18 @@ $(function() {
 $(document).ready(function() {
   $('#logo').addClass('fadeIn');
 });
+
+// load Twitter JavaScript after page is loaded
+$(window).load(function() {
+    !function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.async = true;
+        js.src = p + '://platform.twitter.com/widgets.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    }(document, 'script', 'twitter-wjs');
+  }
+);
